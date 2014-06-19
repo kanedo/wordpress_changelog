969776200d80ec372de78928550512cdfd9b31f3
Gabriel Bretschner <software@kanedo.net>
2014-06-19 19:21:25 +0200

    added View Class for ERB Template

---
+++ lib/wordpress_changelog/view.rb
@@ -0,0 +1,11 @@
+module WordpressChangelog
+	class View
+		def initialize(versionA, versionB, changes)
+			@versionA = versionA
+			@versionB = versionB
+			@changes  = changes
+		end
+		
+		
+	end
+end
