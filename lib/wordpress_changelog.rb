cd7dc9a002cad65f0be7e9e5987166aac3acdd2c
Gabriel Bretschner <software@kanedo.net>
2014-06-19 19:06:55 +0200

    implemented WP Versions

--- lib/wordpress_changelog.rb
+++ lib/wordpress_changelog.rb
@@ -1,9 +1,12 @@
-require "wordpress_changelog/version"
 require'rubygems'
 require'nokogiri'
 require'open-uri'
+require'set'
+
+require "wordpress_changelog/version"
 
 module WordpressChangelog
+
   class Changelog
 
   	def getCodexUrl
@@ -21,7 +24,7 @@ module WordpressChangelog
 
   	def getVersions
   		page = Nokogiri::HTML(open(getVersionsUrl))
-  		puts codex = getCodexUrl
+  		codex = getCodexUrl
   		puts page.css("a[title~='Version']").each { |link| 
   			version = /(\d\.?)+/.match(link['title'])[0] 
   			unless version.nil?
@@ -36,5 +39,42 @@ module WordpressChangelog
   		}
   	end
   	
+    def changesBetween(versionA, versionB)
+      if @versions["#{versionA}"].nil?
+        puts "version #{versionA} does not exist"
+        return nil
+      end
+      if  versionA > versionB
+        puts "version #{versionB} must be greater that #{versionA}"
+        return nil
+      end
+      version_objects = Array.new
+      categories = Set.new
+      changes = Hash.new("")
+      changes["_description_"] = "changes between Wordpress Version #{versionA} and #{versionB}"
+
+      @versions.select { |key| key <= versionB && key >= versionA }.each{|v, url|
+          version = WordpressVersion.new(v, url)
+          version.getCategories.each{|c| categories.add(c)}
+          version_objects.push(version)
+      }
+      categories.each { |e|  
+        version_objects.each{|v|
+          if v.hasCategory?(e)
+            changes[e] += v.getCategory(e)
+          end
+        }
+      }
+      return changes
+    end
+
+    def output(format, changes)
+      outputHTML(changes)
+    end
+
+    def outputHTML(changes)
+      template_file = File.open(File.absolute_path("./wordpress_changelog/templates/changes.erb", __FILE__))
+      puts template_file.read
+    end
   end
 end
--- lib/wordpress_changelog/version.rb
+++ lib/wordpress_changelog/version.rb
@@ -1,3 +1,48 @@
+require'rubygems'
+require'nokogiri'
+require'open-uri'
+
 module WordpressChangelog
   VERSION = "0.0.1"
+
+  class WordpressVersion
+  	def initialize(version, url)
+  		@version 		= version
+  		@url 			= url
+  		@categories 	= Hash.new
+  		@category_names = Hash.new
+  		loadChangedCategories
+  	end
+	
+	def loadChangedCategories
+		page = Nokogiri::HTML(open(@url))
+		page.css("#bodyContent > h3+ul").each{|list|
+			category = list.previous_element;
+			category_id = category.previous_element()['id']
+			category_name = category.element_children[0].text
+			category_name.strip!  
+			@categories[category_id] = list
+			@category_names[category_id] = category_name
+		}
+	end
+	def printCategory(cat)
+		unless @categories[cat].nil?
+			puts @categories[cat]
+		end
+	end
+
+	def hasCategory?(cat)
+		return !@categories[cat].nil?
+	end
+
+	def getCategory(cat)
+		return @categories[cat]
+	end
+
+	def getCategories
+		categories = Array.new
+		@categories.each_key { |key|  categories.push(key)}
+		return categories
+	end
+  end
 end
