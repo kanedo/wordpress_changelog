223dcc8ebdaddea95ba4813caa951b3e25e0a429
Gabriel Bretschner <software@kanedo.net>
2014-06-19 16:28:23 +0200

    added new class wordpress_changelog

--- lib/wordpress_changelog.rb
+++ lib/wordpress_changelog.rb
@@ -1,5 +1,14 @@
 require "wordpress_changelog/version"
 
 module WordpressChangelog
-  # Your code goes here...
+  class WordpressChangelog
+  	def initialize(args)
+  		
+  	end
+
+  	def getVersionsUrl
+  		return "https://codex.wordpress.org/Category:Versions"
+  	end
+  	
+  end
 end
